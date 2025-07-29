5d4d56488763717817ed790fb683c9be1efb4d6c
Luciano Peralta <lucianoperalta002@gmail.com>
2025-07-29 16:23:21 -0300

    agrego codigo

---
+++ tp_Duarte_Peralta.js
@@ -0,0 +1,20 @@
+let prompt = require("prompt-sync")();
+
+let numero = parseInt(prompt("Ingrese un número menor o igual a 50: "));
+
+
+if (Number.isInteger(numero) && numero > 0 && numero <= 50) {
+
+    for (let i = 1; i <= numero; i++) {
+        let linea = "";
+
+        for (let j = 1; j <= i; j++) {
+            linea = linea + j;
+        }
+
+        console.log(linea);
+    }
+
+} else {
+    console.log("Número inválido. Debe ser un número entero entre 1 y 50.");
+}
